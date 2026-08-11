a3760f278e4247887d66145535295a97bc477a0f
Tea <tea@example.com>
2026-08-11 11:57:41 +0200

    Fix: set Authorization header to Bearer  in apiSlice.prepareHeaders

--- frontend/src/redux/api/apiSlice.ts
+++ frontend/src/redux/api/apiSlice.ts
@@ -11,7 +11,7 @@ export const apiSlice = createApi({
       const authState = (getState() as RootState).auth?.user;
       const token = authState?.token ?? authState?.data?.token;
       if (token) {
-        headers.set("authorization", `Bearer `);
+        headers.set(authorization, );
       }
       return headers;
     },
